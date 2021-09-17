f491064b2e2dad432870dec52edd06829502a1e9
Alex Markley <alex@alexmarkley.com>
2021-09-17 01:44:27 +0000

    compress an archive for uploading

--- dobuild.sh
+++ dobuild.sh
@@ -19,7 +19,7 @@ _log Setting up working directories...
 mkdir -p ~/blender-git || _die failed to make build directory
 cd ~/blender-git || _die failed to change to build directory
 
-#_log Cloning blender...
+_log Cloning blender...
 git clone https://github.com/blender/blender.git || _die failed to clone blender
 cd blender || _die failed cd
 git checkout blender-v2.93-release || _die failed checkout
@@ -40,6 +40,13 @@ _log Compiling blender...
 cd ~/blender-git/blender || _die failed to change to build directory
 ### make update || _die failed make update
 make release || _die failed make
+GIT_COMMIT=$(git rev-parse --short HEAD) || _die git rev-parse failed
+
+_log Compressing archive...
+cd ~/blender-git/build_linux_release/bin || _die change to output directory failed
+mv 2.93 blender-2.93-"${GIT_COMMIT}"-linux-x64 || _die rename output directory failed
+tar cJvf blender-2.93-"${GIT_COMMIT}"-linux-x64.tar.xz blender-2.93-"${GIT_COMMIT}"-linux-x64
+_log Created output archive. $(pwd)/blender-2.93-"${GIT_COMMIT}"-linux-x64.tar.xz
 
 _log All done.
 exit 0
